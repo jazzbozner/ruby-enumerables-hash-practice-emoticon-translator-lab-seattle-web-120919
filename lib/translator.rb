87a6663387ce39ce8b50643633c3474ef0e55e7e
Jazz Bozner <jazzbozner@gmail.com>
2019-12-06 00:20:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,6 @@
 
 def load_library
+  def load_library(file_path)
   library = YAML.load_file(file_path)
   result = {"get_meaning" => {}, "get_emoticon" => {}}
   library.each do |meaning, emoticons|
